8c79731e3a0232aeade49e6c4fa1866d69ca2dc1
yash98 <yashmalviya98@gmail.com>
2017-09-05 22:54:35 +0530

    final of 05/09

--- lab5/code/gcd.vhd
+++ lab5/code/gcd.vhd
@@ -27,9 +27,11 @@ signal c: std_logic_vector (0 to 27) := "0000000000000000000000000000";
 signal a: std_logic_vector (7 downto 0);
 signal b: std_logic_vector (7 downto 0);
 
-signal load_i: std_logic;
-signal sub_i: std_logic;
-signal mod_clk: std_logic;
+signal load_i1: std_logic := '0';
+signal sub_i1: std_logic := '0';
+signal load_i2: std_logic := '0';
+signal sub_i2: std_logic := '0';
+signal mod_clk: std_logic := '0';
 begin
     process(clk, pushbutton)
             begin
@@ -54,30 +56,27 @@ begin
         if (a_i(3 downto 0) < ten and a_i(7 downto 4) < ten and b_i(3 downto 0) < ten and b_i(7 downto 4) < ten) then
             op_valid <= '1';
                 if (push_i = '1') then
-                    load_i <= '1';
-                    sub_i <= '0';
-                else load_i <= '0';
+                    load_i1 <= '1';
+                    sub_i1 <= '0';
+                else load_i1 <= '0';
                 end if;
         else
             op_valid <= '0';
-            load_i <= '0';
-            sub_i <= '0';
-        end if;
-        if (load_i = '1') then
-            a <= a_i;
-            b <= b_i;
-            sub_i <= '1';
-            load_i <= '0';
-         end if;
-         if (b = a) then
-             sub_i <= '0';
+            load_i1 <= '0';
+            sub_i1 <= '0';
         end if;
     end process;
     
     process (mod_clk)
     begin
         if (mod_clk = '1' and mod_clk'event) then
-            if (sub_i = '1') then
+            if (load_i1 = '1') then
+                a <= a_i;
+                b <= b_i;
+                sub_i2 <= '1';
+                load_i2 <= '0';
+            end if;
+            if (sub_i2 = '1') then
                 if (a > b) then
                     if a (3 downto 0) < b (3 downto 0) then
                         a <= a(7 downto 4) - b(7 downto 4) - "0001" & a(3 downto 0) - b(3 downto 0) + "1010";
@@ -94,13 +93,17 @@ begin
                             b <= b(7 downto 4) - a(7 downto 4) & b(3 downto 0) - a(3 downto 0);
                             a <= a;
                       end if;
+                 elsif (b = a) then
+                      sub_i2 <= '0';
                 end if;
             end if;
         end if;
     end process;
     
-    sub <= sub_i;
-    load <= load_i;
+    sub <= sub_i1;
+    sub <= sub_i2;
+    load <= load_i1;
+    load <= load_i2;
     d_o <= a & b;
 
 end architecture;
