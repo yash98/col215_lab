31bfda6eaac769efae797ff4dd63c79c2c8554c1
yash98 <yashmalviya98@gmail.com>
2017-09-14 21:03:07 +0530

    lab 5 updated lab 6 added

--- lab5/code/lab5_gcd.vhd
+++ lab5/code/lab5_gcd.vhd
@@ -1214,10 +1214,6 @@ begin
    
 end BEHAVIORAL;
 
-
-
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
@@ -1239,17 +1235,18 @@ end entity;
 
 architecture beh of gcd is
 signal ten: std_logic_vector (3 downto 0) := "1010";
+signal zero: std_logic_vector (3 downto 0) := "0000";
 
 signal t: std_logic:= '0';
 signal eoc: std_logic_vector (0 to 27) := "1011111010111100000111111111";
 signal c: std_logic_vector (0 to 27) := "0000000000000000000000000000";
 
-signal a: std_logic_vector (7 downto 0);
-signal b: std_logic_vector (7 downto 0);
+shared variable a: std_logic_vector (7 downto 0);
+shared variable b: std_logic_vector (7 downto 0);
 
-signal op_valid_i: std_logic := '0';
-signal load_i: std_logic := '0';
-signal sub_i: std_logic := '0';
+shared variable op_valid_i: std_logic := '0';
+shared variable load_i: std_logic := '0';
+shared variable sub_i: std_logic := '0';
 
 --signal op_valid_i1: std_logic := '0';
 --signal load_i1: std_logic := '0';
@@ -1276,58 +1273,71 @@ begin
                 end if;
         end process;
 
-    process (push_i, a_i, b_i, mod_clk, load_i, sub_i)
+    process (push_i, a_i, b_i, mod_clk)
     begin
-        if (a_i(3 downto 0) < ten and a_i(7 downto 4) < ten and b_i(3 downto 0) < ten and b_i(7 downto 4) < ten) then
-            op_valid_i <= '1';
+        if ((a_i(3 downto 0) < ten and a_i(7 downto 4) < ten and b_i(3 downto 0) < ten and b_i(7 downto 4) < ten) and not(a_i(3 downto 0) = zero and a_i(7 downto 4) = zero) and not(b_i(3 downto 0) = zero and b_i(7 downto 4) = zero)) then
+            op_valid_i := '1';
             if (push_i = '1') then
-                load_i <= '1';
-                sub_i <= '0';
+                load_i := '1';
+                sub_i := '0';
+                a := a_i;
+                b := b_i;
+            else
+                load_i := '0';
             end if;
         else
-            op_valid_i <= '0';
-            load_i <= '0';
-            sub_i <= '0';
+            op_valid_i := '0';
+            if (push_i = '1') then
+                load_i := '0';
+                sub_i := '0';
+            end if;
         end if;
+        
+        
         
 
 
         if (mod_clk = '1' and mod_clk'event) then
-            if (load_i = '1') then
-                    a <= a_i;
-                    b <= b_i;
-                    sub_i <= '1';
-                    load_i <= '0';
-            elsif (sub_i = '1') then
+            if (a = a_i and b = b_i) then
+                sub_i := '1';
+            end if;
+            if (sub_i = '1' and load_i = '0') then
                 if (a > b) then
                     if a (3 downto 0) < b (3 downto 0) then
-                        a <= a(7 downto 4) - b(7 downto 4) - "0001" & a(3 downto 0) - b(3 downto 0) + "1010";
-                        b <= b;
+                        a := a(7 downto 4) - b(7 downto 4) - "0001" & a(3 downto 0) - b(3 downto 0) + "1010";
+                        b := b;
                       else
-                          a <= a(7 downto 4) - b(7 downto 4) & a(3 downto 0) - b(3 downto 0);
-                          b <= b;
+                          a := a(7 downto 4) - b(7 downto 4) & a(3 downto 0) - b(3 downto 0);
+                          b := b;
                       end if;
                  elsif (b > a) then
                       if (a (3 downto 0) > b (3 downto 0)) then
-                            b <= b(7 downto 4) - a(7 downto 4) - "0001" & b(3 downto 0) - a(3 downto 0) + "1010";
-                            a <= a;
+                            b := b(7 downto 4) - a(7 downto 4) - "0001" & b(3 downto 0) - a(3 downto 0) + "1010";
+                            a := a;
                       else
-                            b <= b(7 downto 4) - a(7 downto 4) & b(3 downto 0) - a(3 downto 0);
-                            a <= a;
+                            b := b(7 downto 4) - a(7 downto 4) & b(3 downto 0) - a(3 downto 0);
+                            a := a;
                       end if;
-                 elsif (b = a) then
-                      sub_i <= '0';
+                  end if;
+                 if (b = a) then
+                      sub_i := '0';
                 end if;
             end if;
         end if;
+        sub <= sub_i;
+        load <= load_i;
+        d_o <= a & b;
     end process;
     
-    sub <= sub_i;
-    load <= load_i;
+    
+    
     op_valid <= op_valid_i;
-    d_o <= a & b;
+    
 
 end architecture;
+        
+        
+    
 
 
 
@@ -1385,7 +1395,7 @@ begin
         clk => clk,
         load => load,
         sub => sub,
-        op_valid =>op_valid,
+        op_valid => op_valid,
         d_o => d
     );
     
@@ -1398,3 +1408,4 @@ begin
     );
 
 end architecture;
+
