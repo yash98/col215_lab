0e664d76df95fb733189c02d93b3cbd60dc79189
yash98 <yashmalviya98@gmail.com>
2017-11-09 17:50:14 +0530

    crc rsting done

--- Mini Project/src/crc_computation.vhd
+++ Mini Project/src/crc_computation.vhd
@@ -99,6 +99,8 @@ signal zero: std_logic_vector(15 downto 0):="0000000000000000";
 signal c: std_logic_vector(1 downto 0):="00"; -- reading count
 signal n: std_logic_vector(3 downto 0):="00000"; -- shifting cont(during crc comp)
 signal shift: std_logic:='0'; -- to shift indicator
+signal rsting: std_logic:='0';
+signal rst_c: std_logic_vector(3 downto 0):="0000";
 
 begin
 
@@ -107,83 +109,97 @@ begin
     if rising_edge(clk) then
         we_inp <= '0';
         we_crc <= '0';
-        rst_inp <= '0';
-        rst_crc <= '0';
         
-        -- load inputs on ech press
-        if (pb1='1') then
-           addr_inp <= w_inp_addr;
-           wr_inp <= data_inp;
-           we_inp <= '1';
-           w_inp_addr <= w_inp_addr+"0001";
-        end if;
-        
-        -- reset 
-        if (pb2='1') then
-        
-            w_inp_addr <= "0000";
-            we_inp <= '0';
-            rst_inp <= '1';
+        if (rsting='1') then
+                we_inp <= '1';
+                we_crc <= '1';
+                wr_crc <= "1111111111111111";
+                wr_inp <= "1111111111111111";
+                addr_inp <= c;
+                addr_crc <= c;
+                c <= c+"0001";
+            if (rst_c="1111") then
+                rsting <= '0';
+            end if;
+        else
+            -- load inputs on ech press
+            if (pb1='1' and wait_read='0') then
+               addr_inp <= w_inp_addr;
+               wr_inp <= data_inp;
+               we_inp <= '1';
+               w_inp_addr <= w_inp_addr+"0001";
+            end if;
             
-            w_crc_addr <= "0000";
-            we_crc <= '0';
-            rst_crc <= '1';
+            -- reset 
+            if (pb2='1') then
             
-            r_inp_addr <= "0000";
-            
-            done <= '0';
-            wait_read <= '0';
-            start <= '0';
-            shift <= '0';
-            c <= "00";
-            n <= "0000";
-            crc <= "1111111111111111";
-            -- check int signal settings
-        end if;
-        
-        -- start crc comp of next addr
-        if (pb3='1' and start='0' and wait_read='0') then
-            we_inp <= '0';
-            addr_inp <= r_inp_addr;
-            wait_read<='1';
-            r_inp_addr <= r_inp_addr+"0001";
-            done <= '0';
-        end if;
-        
-        -- read into local memory
-        if (wait_read='1') then
-            if (c<"10") then
-                c <= c+"01";
-            else
-                start <= '1';
-                shift <= '0';
-                n <= "0000";
-                c <= "00";
+                w_inp_addr <= "0000";
+                we_inp <= '0';
+                rst_inp <= '1';
+                
+                w_crc_addr <= "0000";
+                we_crc <= '0';
+                rst_crc <= '1';
+                
+                r_inp_addr <= "0000";
+                
+                done <= '0';
                 wait_read <= '0';
-                int_inp <= one & inp_bram_data & zero;
-            end if;
-        end if;
-        
-        -- start calculation
-        if (start='1') then
-            if (shift='1') then
-                int_inp <= int_inp(46 downto 0) & "0";
+                start <= '0';
                 shift <= '0';
-            else
-                if (int_inp(47)='1') then
-                    int_inp <= (int_inp(47 downto 32) xor poly) & int_inp(31 downto 0);
-                end if;
-                shift <= '1';
-                n <= n + "0001";
-                -- 33rd xor has no shift sart to 0 crc displayed
-                if (n="10000") then
-                    start <= '0';
-                    crc <= int_inp(47 to 32);
-                    done <= '1';
+                c <= "00";
+                n <= "0000";
+                crc <= "1111111111111111";
+                
+                rst_c <= "0000";
+                rsting <= '1';
+                -- check int signal settings
+            end if;
+            
+            -- start crc comp of next addr
+            if (pb3='1' and start='0' and wait_read='0') then
+                we_inp <= '0';
+                addr_inp <= r_inp_addr;
+                wait_read<='1';
+                r_inp_addr <= r_inp_addr+"0001";
+                done <= '0';
+            end if;
+            
+            -- read into local memory
+            if (wait_read='1') then
+                if (c<"10") then
+                    c <= c+"01";
+                else
+                    start <= '1';
+                    shift <= '0';
+                    n <= "0000";
+                    c <= "00";
+                    wait_read <= '0';
+                    int_inp <= one & inp_bram_data & zero;
                 end if;
             end if;
+            
+            -- start calculation
+            if (start='1') then
+                if (shift='1') then
+                    int_inp <= int_inp(46 downto 0) & "0";
+                    shift <= '0';
+                else
+                    if (int_inp(47)='1') then
+                        int_inp <= (int_inp(47 downto 32) xor poly) & int_inp(31 downto 0);
+                    end if;
+                    shift <= '1';
+                    n <= n + "0001";
+                    -- 33rd xor has no shift sart to 0 crc displayed
+                    if (n="10000") then
+                        start <= '0';
+                        crc <= int_inp(47 to 32);
+                        done <= '1';
+                    end if;
+                end if;
+            end if;
+            
         end if;
-        
     end if;
 end process;
 
